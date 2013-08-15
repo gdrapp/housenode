33017dacd516cb3178d3b5fb6ccba5f0d93fb14e
Greg Rapp <gdrapp@gmail.com>
2013-08-14 22:34:24 -0400

    Updated cameras to stop streaming when the AngularJS route changes

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -44,10 +44,10 @@ angular.module('houseNode.controllers', []).
     socket.emit('device:getAll');
   }).
   controller('CamerasController', function ($scope, socket) {
-    $scope.$on('$routeChangeStart', function(next, current) { 
-      //window.stop();
-      $("img").attr("src", "#");
-      //$("iframe").remove();
+    $scope.$on('$routeChangeStart', function(next, current) {
+      // Stop the camera streams when the route changes 
+      window.stop();
+      document.execCommand("Stop", false);
     });
   }).
   controller('AdminController', function ($scope, socket) {
