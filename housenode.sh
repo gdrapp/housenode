ffe06d302e4c977fa63a3c95d21b1a234d844889
Greg Rapp <gdrapp@gmail.com>
2013-08-15 21:25:53 -0400

    Updated housenode.sh to use a pid file Minor cleanup in cameras.jade

--- housenode.sh
+++ housenode.sh
@@ -2,10 +2,10 @@
 
 case "$1" in
     start)
-  node_modules/forever/bin/forever start -a -l forever.log -o housenode-stdout.log -e housenode-stderr.log --minUptime 10000 --spinSleepTime 5000 app.js
+  node_modules/forever/bin/forever start --pidFile /var/run/housenode.pid  -a -l forever.log -o housenode-stdout.log -e housenode-stderr.log --minUptime 10000 --spinSleepTime 5000 app.js
         ;;
     stop)
-  node_modules/forever/bin/forever stop app.js
+  node_modules/forever/bin/forever stop --pidFile /var/run/housenode.pid app.js
         ;;
     *)
         echo "Please use start or stop as first argument"
